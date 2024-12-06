68c58ab0a8138a4be8f683f0f2fd73801e72e8d1
Ravishankar Mahto <ravishankarkumar326@yahoo.com>
2024-12-06 06:34:14 +0000

    config file

---
+++ JavaScript/declare.js
@@ -0,0 +1,23 @@
+const accountId = 39186355985;
+const customerId = 3105;
+var accountName = "SubmitEvent"
+let accountEmail = "rsk543782gmail.customer"
+let accountPassword = "8877"
+let accountType;
+
+/*
+using var occurs the scope issue in javascript, scope means write code under curly braces {
+                           ---------------
+}
+
+*/
+
+accountEmail = "ravi24@google.com"
+accountPassword = "89894"
+accountType = "saving"
+accountCity = "Begusarai"
+accountState = "Bihar"
+
+console.log(customerId)
+
+console.table([accountEmail, accountPassword, accountType, accountCity, accountState])
---
+++ JavaScript/hello.js
@@ -0,0 +1,7 @@
+console.log("Hello World");
+console.log("Namaste Dunia")
+console.log("Ravishankar")
+console.log("My name is Ravishankar kumar")
+console.log("I'm 21 year old")
+console.log("a final year student, currently studying in Anna University")
+
