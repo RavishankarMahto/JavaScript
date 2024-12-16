511f75b86aba37376252b2fa243d6fe7abf3a9c5
Ravishankar Mahto <ravishankarkumar326@yahoo.com>
2024-12-16 05:51:53 +0000

    adding files

---
+++ JavaScript/d5_string.js
@@ -0,0 +1,66 @@
+const name = "Raishankar kumar"
+const repoCount = 50
+
+// console.log(name + repoCount + " value") // outdated writing this line of code
+
+// string interpolation
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`) // modern trend writing of code
+
+
+const gameName = new String(`Ravishankar`)
+// console.log(gameName [0]);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase())
+// //String.charAt
+// //  at() method returns the character at a specified index (position) in a string.
+// console.log(gameName.charAt(0)) //R
+// console.log(gameName.charAt(1)) // a
+// console.log(gameName.charAt(2)) // v
+// console.log(gameName.charCodeAt(0)) //82
+
+//String indexOf())
+// console.log(gameName.indexOf('a')) // 1
+// console.log(gameName.indexOf('R')) // 0
+
+
+// slice string
+
+// String length
+// String charAt()
+// String charCodeAt()
+// String at()
+// String [ ]
+// String slice()
+// String substring()
+// String substr()
+// See Also:                   these all are uses in string.
+// String Search Methods
+// String Templates
+// String toUpperCase()
+// String toLowerCase()
+// String concat()
+// String trim()
+// String trimStart()
+// String trimEnd()
+// String padStart()
+// String padEnd()
+// String repeat()
+// String replace()
+// String replaceAll()
+// String split()
+
+
+// String Search Methods:
+// String indexOf()
+// String lastIndexOf()
+// String search()
+// See Also:
+// Basic String Methods
+// String Templates
+// String match()
+// String matchAll()
+// String includes()
+// String startsWith()
+// String endsWith()
+
+
